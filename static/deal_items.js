1ada0ebbe15432a5243943eb70ef289b86dd302e
Kumyl Najeeb <mknajeeb@uwaterloo.ca>
2019-09-16 01:22:23 -0400

    Fix bug causing some deals not to be highlighted

--- static/deal_items.js
+++ static/deal_items.js
@@ -2,7 +2,5 @@ function searchFunction(l) {
     var element = document.getElementById(l["uuid"])
     if (l["alert"] == "true") {
         element.classList.toggle("list-group-item-warning")
-    } else {
-        element.classList.toggle("list-group-item-primary")
     }
 }
