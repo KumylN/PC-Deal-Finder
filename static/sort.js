ca83052422debdc37b9fe5ae8e34d98aa8203efc
Kumyl <m_kumyl@hotmail.com>
2019-10-11 13:16:37 -0400

    WIP

--- static/sort.js
+++ static/sort.js
@@ -1,11 +1,32 @@
-function sortFunction() {
+function changePage(page) {
+
+}
+
+function makePagination(len) {
+    var paginationIndex = document.getElementById('paginationIndex');
+    console.log(len);
+    for (var i = 1; (i < Math.floor(len / 50) + 2); i++) {
+        console.log("Running pagination...")
+        var page = document.createElement("a");
+        var textNode = document.createTextNode(String(i));
+        page.appendChild(textNode);
+        page.setAttribute("onclick", "changePage(" + String(i) + ")");
+        paginationIndex.appendChild(page);
+        console.log("SUCCESS")
+    }
+}
+
+function sortFunction(pageMin=0) {
     var elements = document.getElementsByClassName('deal');
-    
+
     if (elements.length === 0) {
         document.getElementById('deals-list').style.display = "block";
         document.getElementById("NoSearchResults").className += " noSearch";
         return
     }
+
+    makePagination(elements.length);
+
     var array = [];
     for (var i = elements.length >>> 0; i--;) {
         array[i] = elements[i];
